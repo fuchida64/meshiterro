bf3a6a386a2f4a7f8fcba72a2a2069bcca142c3f
fuchida64 <crbsfy@icloud.com>
2018-06-19 04:57:11 +0900

    Finish 5-7

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,21 @@
 class UsersController < ApplicationController
   def show
-  	  @user = User.find(params[:id])
-  	  @post_images = @user.post_images.page(params[:page])
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:page]).reverse_order
   end
 
   def edit
-        @user = User.find(params[:id])
-    end
+    @user = User.find(params[:id])
+  end
+
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
-    redirect_to user_path(@user.id)
-end
-private
-	def user_params
-	    params.require(:user).permit(:name, :profile_image)
-	end
+    redirect_to user_path(@user)
+  end
 
+  private
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,15 @@
 Rails.application.routes.draw do
+  # get 'users/edit' => 'users#edit'
 
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'post_images#index'
+  resources :users
   resources :post_images, only: [:new, :create, :index, :show, :destroy] do
-      resource :favorites, only: [:create, :destroy]
-      resource :post_comments, only: [:create, :destroy]
-       resources :users, only: [:show, :edit, :update]
-end
+  	resource :favorites, only: [:create, :destroy]
+  	resource :post_comments, only: [:create, :destroy]
   end
+
+  root 'post_images#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
+
 Refile.secret_key = '24a182ccdaca6489c330b789f353f63be4156c5eae075951064f0fd37dcdb2e56a4a1e378fb4ab0bb4e9a009ec7479985880a0b471fae431ed8d808d374d442f'
