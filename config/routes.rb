27fc6bd8e6595d98b5e019253b0b598ca85c5860
fuchida64 <crbsfy@icloud.com>
2018-06-18 09:55:59 +0900

    Finish 5-3

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'post_images#index'
+  resources :post_images, only: [:new, :create, :index, :show]
 end
+Refile.secret_key = '24a182ccdaca6489c330b789f353f63be4156c5eae075951064f0fd37dcdb2e56a4a1e378fb4ab0bb4e9a009ec7479985880a0b471fae431ed8d808d374d442f'
